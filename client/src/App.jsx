d53a422b17ed4bf0fda84abd116383f505689ce7
Himasnhu <himanshubhartimax@gmail.com>
2024-05-24 16:21:57 +0530

    add functionality to the signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { About, Dashboard, Home, Projects, SignIn, SignUp, } from "./pages"
 import { Header } from "./components"
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,64 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
+    const [formData, setFormData] = useState({});
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+
+    const handleChange = (e) => {
+        console.log(e.target.value)
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    };
+    console.log(formData);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!formData.username && !formData.email && !formData.password) {
+            return setErrorMessage("Please fill out all fields")
+        }
+
+        // Validate form fields and provide specific error messages
+        if (!formData.username) {
+            return setErrorMessage("Username is required");
+        }
+        if (!formData.email) {
+            return setErrorMessage("Email is required");
+        }
+        if (!formData.password) {
+            return setErrorMessage("Password is required");
+        }
+
+        try {
+            setLoading(true);
+            setErrorMessage(null);
+            const res = await fetch("/api/auth/signup", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(formData)
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                return setErrorMessage(data.message)
+            }
+            setLoading(false);
+            if (res.ok) {
+                navigate("/sign-in");
+            }
+        } catch (error) {
+            setErrorMessage(error.message)
+            setLoading(false);
+        }
+    }
+
     return (
         <div className='min-h-screen mt-20'>
             <div className="flex p-3 max-w-4xl mx-auto flex-col md:flex-row md:items-center gap-5">
                 {/* Left Section */}
-                <div className="flex-1">
+                <div className="flex-1 text-center md:text-left">
                     <Link to={"/"} className='font-bold dark:text-white text-4xl'>
                         <span className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-full px-5 py-2.5 text-center me-2 mb-2'>IBlogIt</span>
                     </Link>
@@ -15,27 +66,45 @@ export default function SignUp() {
                 </div>
                 {/* Right Section */}
                 <div className="flex-1">
-                    <form className='flex flex-col gap-4'>
+                    <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
                         <div>
                             <Label value='Your username' />
-                            <TextInput type='text' placeholder='Username' id='username' />
+                            <TextInput type='text' placeholder='Username' id='username' onChange={handleChange} />
                         </div>
                         <div>
                             <Label value='Your email' />
-                            <TextInput type='email' placeholder='Email' id='username' />
+                            <TextInput type='email' placeholder='Email' id='email' onChange={handleChange} />
                         </div>
                         <div>
                             <Label value='Your password' />
-                            <TextInput type='password' placeholder='Password' id='username' />
+                            <TextInput type='password' placeholder='Password' id='password' onChange={handleChange} />
                         </div>
-                        <Button className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-center ' type='submit'>
-                            Sign Up
+                        <Button
+                            className='text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg text-center '
+                            type='submit'
+                            disabled={loading}
+                        >
+                            {
+                                loading ? (
+                                    <>
+                                        <Spinner size="sm" />
+                                        <span className='pl-3'>Loading...</span>
+                                    </>
+                                ) : "Sign Up"
+                            }
                         </Button>
                     </form>
                     <div className="flex gap-2 text-sm mt-5">
                         <span>Have an account ?</span>
                         <Link to={"/sign-in"} className='text-blue-500'>Sign In</Link>
                     </div>
+                    {
+                        errorMessage && (
+                            <Alert className='mt-5' color="failure">
+                                {errorMessage}
+                            </Alert>
+                        )
+                    }
                 </div>
             </div>
         </div>
