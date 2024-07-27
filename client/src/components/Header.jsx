31dbc2f5555b7a1af5e465427270c61a8593063f
Himasnhu <himanshubhartimax@gmail.com>
2024-07-28 03:45:14 +0530

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Button, Navbar, TextInput } from "flowbite-react"
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react"
 import { Link, useLocation } from "react-router-dom"
 import { AiOutlineSearch } from "react-icons/ai"
 import { FaMoon } from "react-icons/fa"
+import { useSelector } from "react-redux"
 
 export default function Header() {
     const path = useLocation().pathname
+    const { currentUser } = useSelector(state => state?.user)
     return (
         <Navbar className='border-b-2'>
             <Link to={"/"} className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -26,13 +28,40 @@ export default function Header() {
                 <Button className='w-12 h-10 hidden sm:inline' color="gray" pill>
                     <FaMoon className='text-lg' />
                 </Button>
-                <Link to={"/sign-in"}>
-                    <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0">
-                            Sign in
-                        </span>
-                    </button>
-                </Link>
+                {
+                    currentUser ? (
+                        <Dropdown
+                            arrowIcon={false}
+                            inline
+                            label={
+                                <Avatar
+                                    alt='user'
+                                    img={currentUser.profilePicture}
+                                    rounded
+                                />
+                            }
+                        >
+                            <Dropdown.Header>
+                                <span className='block text-sm'>@{currentUser.username}</span>
+                                <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                            </Dropdown.Header>
+                            <Link to={"/dashboard?tab=profile"}>
+                                <Dropdown.Item>Profile</Dropdown.Item>
+                            </Link>
+                            <Dropdown.Divider />
+                            <Dropdown.Item>Sign out</Dropdown.Item>
+                        </Dropdown>
+                    ) : (
+                        <Link to={"/sign-in"}>
+                            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0">
+                                    Sign in
+                                </span>
+                            </button>
+                        </Link>
+                    )
+                }
+
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse>
