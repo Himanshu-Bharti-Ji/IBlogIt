e370bdea2d51a3f847779ecf737bd0ce622c312d
Himasnhu <himanshubhartimax@gmail.com>
2024-09-09 22:57:27 +0530

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -19,7 +19,7 @@ export const updateUser = async (req, res, next) => {
     }
 
     if (req.body.username) {
-        if (req.body.username.length < 7 || req.body.username.length > 20) {
+        if (req.body.username.length < 4 || req.body.username.length > 20) {
             return next(errorHandler(400, "Username must be between 7 and 20 characters"))
         }
 
@@ -40,23 +40,19 @@ export const updateUser = async (req, res, next) => {
                 400, "Username can only contain letters and numbers"
             ))
         }
-
-        try {
-            const updateUser = await User.findByIdAndUpdate(req.params.userId, {
-                $set: {
-                    username: req.body.username,
-                    email: req.body.email,
-                    profilePicture: req.body.profilePicture,
-                    password: req.body.password
-                },
-            }, { new: true })
-            const { password, ...rest } = updateUser._doc;
-            res.status(200).json(rest);
-        } catch (error) {
-            next(error)
-        }
     }
-
-
-
+    try {
+        const updateUser = await User.findByIdAndUpdate(req.params.userId, {
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                profilePicture: req.body.profilePicture,
+                password: req.body.password
+            },
+        }, { new: true })
+        const { password, ...rest } = updateUser._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
 };
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -23,9 +23,22 @@ export const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        updateStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        updateSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateFaliure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        }
     }
 })
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure, updateStart, updateSuccess, updateFaliure } = userSlice.actions;
 
 export default userSlice.reducer;
