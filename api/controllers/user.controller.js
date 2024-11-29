09013a63ac62876578eaa2eae1a3a3dffb2221ed
Himasnhu <himanshubhartimax@gmail.com>
2024-11-29 22:15:41 +0530

    Add getUsers endpoint to backend

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -78,4 +78,37 @@ export const signout = async (_, res, next) => {
     } catch (error) {
         next(error);
     }
+}
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+        const users = await User.find().sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit).select("-password");
+
+        const totalUsers = await User.countDocuments();
+
+        const now = new Date();
+
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+
+        const lastMonthUsers = await User.countDocuments({
+            createdAt: { $gte: oneMonthAgo }
+        });
+
+        res.status(200).json({
+            users,
+            totalUsers,
+            lastMonthUsers
+        });
+
+    } catch (error) {
+        next(error)
+    }
 }  
