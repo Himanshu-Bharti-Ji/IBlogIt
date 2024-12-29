410ebe0ba0975f97e28a3c2379a225e6dc76a447
Himasnhu <himanshubhartimax@gmail.com>
2024-12-29 15:31:28 +0530

    Add getComments endpoint and comment section to admin to view all comments

---
+++ client/src/components/DashComments.jsx
@@ -0,0 +1,165 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Button, Modal, Table } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { FaCheck, FaTimes } from "react-icons/fa";
+
+const DashComments = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [comments, setComments] = useState([]);
+  const [showMore, setShowMore] = useState(true);
+  const [showModel, setShowModel] = useState(false);
+  const [commentIdToDelete, setCommentIdToDelete] = useState("");
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/get-comments`);
+        const data = await res.json();
+        if (res.ok) {
+          setComments(data.comments);
+          if (data.length < 9) {
+            setShowMore(false);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchComments();
+    }
+  }, [currentUser._id, currentUser.isAdmin]);
+
+  const handleShowMore = async () => {
+    const startIndex = comments.length;
+
+    try {
+      const res = await fetch(
+        `/api/comment/get-comments?startIndex=${startIndex}`
+      );
+
+      const data = await res.json();
+      if (res.ok) {
+        setComments([...comments, ...data.comments]);
+        if (data.comments.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleDeleteComment = async () => {
+    try {
+      const res = await fetch(
+        `api/comment/deleteComment/${commentIdToDelete}`,
+        {
+          method: "DELETE",
+        }
+      );
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data?.message);
+      } else {
+        setComments((prev) =>
+          prev.filter((comment) => comment._id !== commentIdToDelete)
+        );
+        setShowModel(false);
+      }
+    } catch (error) {
+      console.log(data?.message);
+    }
+  };
+
+  return (
+    <div className="w-full table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
+      {currentUser.isAdmin && comments.length > 0 ? (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Comment content</Table.HeadCell>
+              <Table.HeadCell>Number of likes</Table.HeadCell>
+              <Table.HeadCell>post ID</Table.HeadCell>
+              <Table.HeadCell>User ID</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+            </Table.Head>
+            {comments?.map((comment) => (
+              <Table.Body key={comment?._id} className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    {new Date(comment?.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>{comment?.content}</Table.Cell>
+                  <Table.Cell>
+                    <div className="font-medium text-gray-900 dark:text-white">
+                      {comment?.numberOfLikes}
+                    </div>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <div className="font-medium text-gray-900 dark:text-white">
+                      {comment?.postId}
+                    </div>
+                  </Table.Cell>
+                  <Table.Cell>{comment?.userId}</Table.Cell>
+                  <Table.Cell>
+                    <span
+                      onClick={() => {
+                        setShowModel(true);
+                        setCommentIdToDelete(comment?._id);
+                      }}
+                      className="font-medium text-red-500 hover:underline cursor-pointer"
+                    >
+                      Delete
+                    </span>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+          {showMore && (
+            <div className="w-full text-center py-7">
+              <button
+                onClick={handleShowMore}
+                className="text-teal-500  text-md font-semibold hover:underline"
+              >
+                Show More
+              </button>
+            </div>
+          )}
+        </>
+      ) : (
+        <p>You have no comments yet!</p>
+      )}
+      <Modal
+        show={showModel}
+        onClose={() => setShowModel(false)}
+        popup
+        size={"lg"}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
+            <h3 className="mb-5 text-xl text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this comment ?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color={"failure"} onClick={handleDeleteComment}>
+                Yes, I'm sure
+              </Button>
+              <Button color={"gray"} onClick={() => setShowModel(false)}>
+                No, Cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default DashComments;
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -5,6 +5,7 @@ import {
   HiDocumentText,
   HiUser,
   HiOutlineUserGroup,
+  HiAnnotation,
 } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
@@ -72,6 +73,15 @@ export default function DashSidebar() {
                   as="div"
                 >
                   Users
+                </Sidebar.Item>
+              </Link>
+              <Link to={"/dashboard?tab=comments"}>
+                <Sidebar.Item
+                  active={tab === "users"}
+                  icon={HiAnnotation}
+                  as="div"
+                >
+                  Comments
                 </Sidebar.Item>
               </Link>
             </>
