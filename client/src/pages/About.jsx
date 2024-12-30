51942bdd61bf50ed1fd996796609f3694f91f061
Himasnhu <himanshubhartimax@gmail.com>
2024-12-30 21:52:20 +0530

    Complete About and Projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
-    return (
-        <div>
-            About
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center p-3 max-w-4xl mx-auto">
+        <h1 className="text-3xl font-semibold my-7">About IBlogIt</h1>
+        <div className=" text-gray-500 text-lg flex flex-col gap-6">
+          <p>
+            IBlogIt is a vibrant platform dedicated to empowering individuals to
+            share their unique stories, knowledge, and experiences with the
+            world. Whether you are a seasoned blogger or just starting your
+            journey, IBlogIt provides an intuitive and feature-rich environment
+            to craft and publish compelling content effortlessly. Our mission is
+            to foster a community where voices are heard, and creativity
+            thrives.
+          </p>
+          <p>
+            At IBlogIt, we believe that every story has the potential to
+            inspire, educate, and connect people globally. With a seamless user
+            experience, powerful tools, and personalized features, we aim to
+            make blogging accessible to everyone. Explore a wide array of
+            categories, from technology to lifestyle, and discover a world of
+            ideas waiting to be explored.
+          </p>
+          <p>
+            Join our growing community of bloggers and readers who are
+            passionate about making a difference through the power of words.
+            Whether you're here to share your thoughts or find inspiration,
+            IBlogIt is your ultimate destination to connect, learn, and grow.
+            Start your blogging adventure with us today and let your voice be
+            the spark that ignites change.
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
-    return (
-        <div>
-            Projects
-        </div>
-    )
+  return (
+    <div className="min-h-screen p-3 max-w-4xl mx-auto flex flex-col justify-center items-center gap-6 text-center">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-gray-500 text-lg">
+        Dive into some of the most exciting projects I’ve worked on! Each one
+        showcases my dedication to creating user-centric designs and efficient
+        solutions, reflecting my journey as a passionate frontend developer.
+        Explore my portfolio to see more of my work and the technologies I
+        specialize in.
+      </p>
+
+      <CallToAction />
+    </div>
+  );
 }
